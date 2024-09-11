97af059d861c97671133cbf94897928c85d834f4
Leonardo Dias Brito <leonardobrito_2002@hotmail.com>
2024-09-11 04:18:11 -0300

    Initial commit

---
+++ src/desComissao/Comissao.java
@@ -0,0 +1,25 @@
+package desComissao;
+import java.util.Scanner;
+
+public class Comissao {
+    public static void main(String[] args) {
+        int salario = 2000;
+        double porcentagem = 2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor total de vendas realizadas no mês");
+        double comissao = scanner.nextInt();
+        if (comissao < 100000) {
+            comissao = (porcentagem / 100) * comissao;
+            double total = salario + comissao;
+            System.out.println("O seu salario é R$" +total+ ", sendo R$ " +comissao+ " de comissão.");
+        }
+        if (comissao <55000) {
+            salario = salario + 100;
+            System.out.println("O seu salario é R$" +salario);
+        }
+        if (comissao >100000) {
+            salario = salario + 5000;
+            System.out.println("O seu salario é R$" + salario);
+        }
+    }
+}
