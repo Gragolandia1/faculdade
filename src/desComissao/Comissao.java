963ac72cc60eeae979908541276a3e948b89dd15
Leonardo Dias Brito <leonardobrito_2002@hotmail.com>
2024-09-12 22:55:35 -0300

    update desComissao

--- src/desComissao/Comissao.java
+++ src/desComissao/Comissao.java
@@ -1,30 +1,35 @@
 package desComissao;
-import java.util.Scanner;
 
-//Um calculo de comissão com base no valor total de vendas.
+public class Comissao {
+    private int salario;
+    private double comissao;
 
-public class Comissao extends User {
-    public static void main(String[] args) {
-        Comissao user = new Comissao();
-
-        Scanner scanner = new Scanner(System.in);
-
-        user.setSalario(2000);
-
-        System.out.println("Digite o valor total de vendas realizadas no mês");
-        user.setComissao(scanner.nextInt());
-        if (user.getComissao() <55000) {
-            user.setSalario(user.getSalario() + 100);
-            System.out.println("O seu salario é R$" + user.getSalario());
-        }else if (user.getComissao() < 100000) {
-            user.setComissao(0.02 * user.getComissao());
-            user.setTotal(user.getSalario() + user.getComissao());
-            System.out.println("O seu salario é R$" +user.getTotal()+ ", sendo R$ " +user.getComissao()+ " de comissão.");
-        }
-        if (user.getComissao() >100000) {
-            user.setSalario(user.getSalario() + 5000);
-            System.out.println("O seu salario é R$" + user.getSalario());
-        }
-
+    public double getComissao() {
+        return comissao;
     }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    private double total;
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+
+
 }
--- src/desComissao/User.java
+++ src/desComissao/User.java
@@ -1,35 +1,30 @@
 package desComissao;
+import java.util.Scanner;
 
-public class User {
-    private int salario;
-    private double comissao;
+//Um calculo de comissão com base no valor total de vendas.
 
-    public double getComissao() {
-        return comissao;
+public class User extends Comissao {
+    public static void main(String[] args) {
+        User user = new User();
+
+        Scanner scanner = new Scanner(System.in);
+
+        user.setSalario(2000);
+
+        System.out.println("Digite o valor total de vendas realizadas no mês");
+        user.setComissao(scanner.nextInt());
+        if (user.getComissao() <55000) {
+            user.setSalario(user.getSalario() + 100);
+            System.out.println("O seu salario é R$" + user.getSalario());
+        }else if (user.getComissao() < 100000) {
+            user.setComissao(0.02 * user.getComissao());
+            user.setTotal(user.getSalario() + user.getComissao());
+            System.out.println("O seu salario é R$" +user.getTotal()+ ", sendo R$ " +user.getComissao()+ " de comissão.");
+        }
+        if (user.getComissao() >100000) {
+            user.setSalario(user.getSalario() + 5000);
+            System.out.println("O seu salario é R$" + user.getSalario());
+        }
+
     }
-
-    public void setComissao(double comissao) {
-        this.comissao = comissao;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    private double total;
-
-    public int getSalario() {
-        return salario;
-    }
-
-    public void setSalario(int salario) {
-        this.salario = salario;
-    }
-
-
-
 }
