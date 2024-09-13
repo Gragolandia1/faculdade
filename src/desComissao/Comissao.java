9776605a2798d5fbc0971ac26da47a46000d12c1
Leonardo Dias Brito <leonardobrito_2002@hotmail.com>
2024-09-12 22:50:23 -0300

    update desComissao

--- src/desComissao/Comissao.java
+++ src/desComissao/Comissao.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 
 //Um calculo de comissão com base no valor total de vendas.
 
-public class Comissao {
+public class Comissao extends User {
     public static void main(String[] args) {
-        int salario = 2000;
-        double porcentagem = 2;
+        Comissao user = new Comissao();
+
         Scanner scanner = new Scanner(System.in);
+
+        user.setSalario(2000);
+
         System.out.println("Digite o valor total de vendas realizadas no mês");
-        double comissao = scanner.nextInt();
-        if (comissao < 100000) {
-            comissao = (porcentagem / 100) * comissao;
-            double total = salario + comissao;
-            System.out.println("O seu salario é R$" +total+ ", sendo R$ " +comissao+ " de comissão.");
+        user.setComissao(scanner.nextInt());
+        if (user.getComissao() <55000) {
+            user.setSalario(user.getSalario() + 100);
+            System.out.println("O seu salario é R$" + user.getSalario());
+        }else if (user.getComissao() < 100000) {
+            user.setComissao(0.02 * user.getComissao());
+            user.setTotal(user.getSalario() + user.getComissao());
+            System.out.println("O seu salario é R$" +user.getTotal()+ ", sendo R$ " +user.getComissao()+ " de comissão.");
         }
-        if (comissao <55000) {
-            salario = salario + 100;
-            System.out.println("O seu salario é R$" +salario);
+        if (user.getComissao() >100000) {
+            user.setSalario(user.getSalario() + 5000);
+            System.out.println("O seu salario é R$" + user.getSalario());
         }
-        if (comissao >100000) {
-            salario = salario + 5000;
-            System.out.println("O seu salario é R$" + salario);
-        }
+
     }
 }
---
+++ src/desComissao/User.java
@@ -0,0 +1,35 @@
+package desComissao;
+
+public class User {
+    private int salario;
+    private double comissao;
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    private double total;
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+
+
+}
